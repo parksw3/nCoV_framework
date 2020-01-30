d430b36752a579cf34cc3bff2c24015f0468f244
Ben Bolker <bbolker@gmail.com>
2020-01-30 15:50:08 -0500

    fig 3; tweaking

--- compare_R0.R
+++ compare_R0.R
@@ -1,5 +1,6 @@
 library(ggplot2); theme_set(theme_bw())
 library(gridExtra)
+library(colorspace)
 library(dplyr)
 source("published_estimate.R")
 source("study_number.R")
@@ -46,17 +47,23 @@ R0all %>%
     ww=width>bwidth
   )
 
+all_col <- "#000000" ## black
 g1 <- ggplot(R0all) +
-  geom_hline(data=R0_all, aes(yintercept=lwr), lty=2, col="#C77CFF") +
-  geom_hline(data=R0_all, aes(yintercept=upr), lty=2, col="#C77CFF") +
+  ## tried to collapse these two lines to one using annotate or other clever
+  ## tricks, failed ...
+  geom_hline(data=R0_all, aes(yintercept=lwr), lty=2, col=all_col, alpha=0.5) +
+  geom_hline(data=R0_all, aes(yintercept=upr), lty=2, col=all_col, alpha=0.5) +
   geom_point(aes(anon, est, col=type), position=position_dodge(0.5)) +
   geom_errorbar(aes(anon, ymin=lwr, ymax=upr, col=type), position=position_dodge(0.5),
                 width=0) +
   scale_y_continuous("Basic reproductive number", breaks=c(2, 4, 6, 8, 10)) +
+  ## scale_colour_discrete_qualitative() +
+  scale_colour_manual(values=c(colorspace::qualitative_hcl(4),"#000000")) +
   theme(
     axis.title.x = element_blank(),
     legend.position = "top",
     legend.title = element_blank()
   )
+
 
 ggsave("compare_R0.pdf", g1, width=6, height=4)
